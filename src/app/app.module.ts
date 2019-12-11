ed1b547319642be5bd84c092fcee4c3562c36804
Marcelo Badet <marcelo.badet@araujo.com.br>
2019-12-11 07:51:37 -0300

    Criando so componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ConversorModule } from './conversor';
 
@@ -10,7 +12,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, ConversorModule
+    BrowserModule, ConversorModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
